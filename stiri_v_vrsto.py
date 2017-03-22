8f9a8337637078db71d30ef46f08e3be4f203825
Ines Sovdat <sovdat.ines@gmail.com>
2017-03-22 13:37:56 +0100

    Ustvarili igralno ploščo.

---
+++ stiri_v_vrsto.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+class Gui():
+    TAG_FIGURA = 'figura'
+
+    TAG_OKVIR = 'okvir'
+
+    VELIKOST_POLJA = 50
+
+    POLMER_KROGCA = VELIKOST_POLJA // 2 - 2
+
+    ODMIK = 2
+
+    def __init__(self, master):
+        self.plosca = Canvas(master, width= 7* Gui.VELIKOST_POLJA + Gui.ODMIK, height=6* Gui.VELIKOST_POLJA + Gui.ODMIK)
+        self.plosca.pack()
+        self.narisi_crte()
+        self.plosca.bind("<Button-1>", self.plosca_klik)
+    
+    def narisi_crte(self):
+        """Nariši črte v igralnem polju"""
+        self.plosca.delete(Gui.TAG_OKVIR)
+        d = Gui.VELIKOST_POLJA
+        r = Gui.ODMIK
+        for vrstica in range(0,7):
+            self.plosca.create_line(r , r + vrstica*d ,r + 7*d,r + vrstica*d, tag=Gui.TAG_OKVIR)
+        for stolpec in range(0,8):
+            self.plosca.create_line(r + stolpec * d, r,r + stolpec*d,r + 6*d, tag=Gui.TAG_OKVIR)
+
+    def plosca_klik(self,event):
+        stolpec = (event.x - Gui.ODMIK) // Gui.VELIKOST_POLJA
+        vrstica = (event.y - Gui.ODMIK) // Gui.VELIKOST_POLJA
+        sredisce_stolpec = stolpec * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
+        sredisce_vrstica = vrstica * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
+        self.plosca.create_oval(sredisce_stolpec - Gui.POLMER_KROGCA, sredisce_vrstica - Gui.POLMER_KROGCA, sredisce_stolpec + Gui.POLMER_KROGCA, sredisce_vrstica + Gui.POLMER_KROGCA)
+
+     
+    
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Stiri v vrsto")
+    aplikacija = Gui(root)
+    root.mainloop()
