cddd296944fd9b131aa1ffd5d10492ac30de8da6
Ines Sovdat <sovdat.ines@gmail.com>
2017-03-29 15:25:33 +0200

    Dokončali igralno ploščo

--- stiri_v_vrsto.py
+++ stiri_v_vrsto.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from igra import *
 
 class Gui():
     TAG_FIGURA = 'figura'
@@ -12,11 +13,37 @@ class Gui():
     ODMIK = 2
 
     def __init__(self, master):
-        self.plosca = Canvas(master, width= 7* Gui.VELIKOST_POLJA + Gui.ODMIK, height=6* Gui.VELIKOST_POLJA + Gui.ODMIK)
+        master.protocol("WM_DELETE_WINDOW", lambda: self.zapri_okno(master))
+
+        self.plosca = Canvas(master, width= 7 * Gui.VELIKOST_POLJA + Gui.ODMIK, height= 6 * Gui.VELIKOST_POLJA + Gui.ODMIK)
         self.plosca.pack()
         self.narisi_crte()
         self.plosca.bind("<Button-1>", self.plosca_klik)
-    
+
+        self.igra = Igra()
+
+        # Glavni menu
+        menu = Menu(master)
+        master.config(menu=menu)
+
+        # Podmenu
+
+        menu_igra = Menu(menu, tearoff = 0)
+        menu.add_cascade(label="Igra", menu=menu_igra)
+        menu_igra.add_command(label = "Nova igra", command = self.zacni_igro)
+
+        self.zacni_igro()
+
+    def zacni_igro(self):
+        self.plosca.delete(Gui.TAG_FIGURA)
+
+    def koncaj_igro(self):
+        self.napis.set("Konec igre.")
+
+    def zapri_okno(self, master):
+        # TO DO
+        master.destroy()
+
     def narisi_crte(self):
         """Nariši črte v igralnem polju"""
         self.plosca.delete(Gui.TAG_OKVIR)
@@ -27,15 +54,16 @@ class Gui():
         for stolpec in range(0,8):
             self.plosca.create_line(r + stolpec * d, r,r + stolpec*d,r + 6*d, tag=Gui.TAG_OKVIR)
 
-    def plosca_klik(self,event):
+    def plosca_klik(self, event):
         stolpec = (event.x - Gui.ODMIK) // Gui.VELIKOST_POLJA
         vrstica = (event.y - Gui.ODMIK) // Gui.VELIKOST_POLJA
         sredisce_stolpec = stolpec * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
+        #spremeniti vrstico
         sredisce_vrstica = vrstica * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
-        self.plosca.create_oval(sredisce_stolpec - Gui.POLMER_KROGCA, sredisce_vrstica - Gui.POLMER_KROGCA, sredisce_stolpec + Gui.POLMER_KROGCA, sredisce_vrstica + Gui.POLMER_KROGCA)
+        barva = 'yellow' if self.igra.na_potezi == RUMENI_IGRALEC else 'red'
+        self.plosca.create_oval(sredisce_stolpec - Gui.POLMER_KROGCA, sredisce_vrstica - Gui.POLMER_KROGCA, sredisce_stolpec + Gui.POLMER_KROGCA, sredisce_vrstica + Gui.POLMER_KROGCA, fill = barva)
 
-     
-    
+    #def povleci_potezo():
 
 
 if __name__ == "__main__":
