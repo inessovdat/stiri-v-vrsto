1215300894a587f9d2e22c2bc8d8d46ae8f9bcfb
Ines Sovdat <sovdat.ines@gmail.com>
2017-04-09 23:42:33 +0200

    Popravili določanje zmagovalca in dodali komentarje

--- stiri_v_vrsto.py
+++ stiri_v_vrsto.py
@@ -11,9 +11,11 @@ class Gui():
 
     VELIKOST_POLJA = 70
 
+    # Odmik od roba polja
+    ODMIK = 2
+
     POLMER_KROGCA = VELIKOST_POLJA // 2 - 2
 
-    ODMIK = 2
 
     def __init__(self, master):
         self.rdeci_igralec = None
@@ -21,11 +23,15 @@ class Gui():
 
         master.protocol("WM_DELETE_WINDOW", lambda: self.zapri_okno(master))
 
+        # Igralna plošča
         self.plosca = Canvas(master, width = 7 * Gui.VELIKOST_POLJA + Gui.ODMIK, height = 6 * Gui.VELIKOST_POLJA + Gui.ODMIK, bg = 'blue')
         self.plosca.grid(row=1, column=0)
         self.plosca.bind("<Button-1>", self.plosca_klik)
         self.narisi_igralno_plosco()
+
         self.igra = Igra()
+
+        # Napis ob začetku igre
         self.napis = StringVar(master, value="Dobrodošli v 4 v vrsto!")
         self.w = Label(master, textvariable = self.napis)
         self.w.grid(row=0, column=0)
@@ -41,7 +47,10 @@ class Gui():
 
         self.zacni_igro()
 
+
     def zacni_igro(self):
+        ''' Stanje igre nastavi na začetek.'''
+        # Ustavimo vsa vlakna, ki trenutno razmišljajo
         self.prekini_igralce()
         self.plosca.delete(Gui.TAG_FIGURA)
          # Ustvarimo novo igro
@@ -49,44 +58,52 @@ class Gui():
         # Nastavimo igralce
         self.rdeci_igralec = Clovek(self)
         self.rumeni_igralec = Clovek(self)
-        # Rdei je prvi na potezi
+        # Rdeči igralec je prvi na potezi
         self.napis.set("Na potezi je rdeči igralec.")
         self.rdeci_igralec.igraj()
 
     def koncaj_igro(self, zmagovalec, stirka):
-        if zmagovalec == RDECI_IGRALEC:
+         ''' Stanje igre nastavi na konec. '''
+         if zmagovalec == RDECI_IGRALEC:
+            self.napis.set("Zmagal je rdeči igralec.")
+            self.narisi_zmagovalne_stiri(zmagovalec, stirka)
+         elif zmagovalec == RUMENI_IGRALEC:
             self.napis.set("Zmagal je rumeni igralec.")
             self.narisi_zmagovalne_stiri(zmagovalec, stirka)
-        elif zmagovalec == RUMENI_IGRALEC:
-            self.napis.set("Zmagal je rdeci igralec.")
-            self.narisi_zmagovalne_stiri(zmagovalec, stirka)
-        else:
+         else:
             self.napis.set("Neodločeno.")
 
     def prekini_igralce(self):
+        ''' Pove igralcem naj nehajo z razmišljanjem. '''
         logging.debug ("prekinjam igralce")
         if self.rdeci_igralec: self.rdeci_igralec.prekini()
         if self.rumeni_igralec: self.rumeni_igralec.prekini()
 
 
     def zapri_okno(self, master):
+        ''' Ko uporabnik zapre aplikacijo, ta metoda zapre okno.'''
         self.prekini_igralce()
         master.destroy()
 
+
+
     def narisi_igralno_plosco(self):
+        ''' Nariše igralno ploščo.'''
         for i in range(7):
            for j in range(6):
                self.plosca.create_oval(2 * Gui.ODMIK + i * Gui.VELIKOST_POLJA, 2 * Gui.ODMIK + j * Gui.VELIKOST_POLJA,
                                        (i+1) * Gui.VELIKOST_POLJA, (j+1) * Gui.VELIKOST_POLJA, fill = 'black')
 
-    def narisi_zeton(self,p,barva):
-        (stolpec, sredisce_stolpec,vrstica) = p
+    def narisi_krogec(self, p, barva):
+        '''Na ustrezno mesto nariše krogec ustrezne barve.'''
+        (stolpec, sredisce_stolpec, vrstica) = p
         sredisce_vrstica = vrstica * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
         self.plosca.create_oval(sredisce_stolpec - Gui.POLMER_KROGCA, sredisce_vrstica - Gui.POLMER_KROGCA,
                                         sredisce_stolpec + Gui.POLMER_KROGCA, sredisce_vrstica + Gui.POLMER_KROGCA, fill = barva)
 
     def narisi_zmagovalne_stiri(self, zmagovalec, stirka):
-        barva = 'red' if zmagovalec == RUMENI_IGRALEC else 'yellow'
+        '''S temnejšo barvo obarva in obrobi zmagovalno štirko.'''
+        barva = 'red' if zmagovalec == RDECI_IGRALEC else 'yellow'
         for p in stirka:
             (vrstica, stolpec) = p
             y = vrstica * Gui.VELIKOST_POLJA
@@ -95,6 +112,7 @@ class Gui():
             self.plosca.create_oval(x + z, y + z, x + Gui.VELIKOST_POLJA + z, y + Gui.VELIKOST_POLJA + z, width = 2 * Gui.ODMIK, fill = barva)
 
     def plosca_klik(self, event):
+        ''' Izvede se ob kliku na igralno ploščo. '''
         stolpec = (event.x - Gui.ODMIK) // Gui.VELIKOST_POLJA
         sredisce_stolpec = stolpec * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
         vrstica = self.igra.vrni_vrstico(stolpec)
@@ -105,15 +123,20 @@ class Gui():
         igralec = self.igra.na_potezi
         r = self.igra.shrani_poteze(stolpec)
         (zmagovalec, stirka) = r
-        if r == None:
+        if r is None:
+            # Neveljavna poteza, nič se ne spremeni
             pass
         else:
+            # Veljavna poteza znotraj igralne plošče
             if vrstica != None:
+                # Na zaslon narišemo krogec ustrezne barve
                 if igralec == RDECI_IGRALEC:
-                    self.narisi_zeton(p, 'gold')
+                    self.narisi_krogec(p, 'tomato')
                 else:
-                    self.narisi_zeton(p, 'tomato')
+                    self.narisi_krogec(p, 'gold')
+                # Igre še ni konec
                 if zmagovalec == NI_KONEC:
+                    # Izvede se naslednja poteza in zamenja napis
                     if self.igra.na_potezi == RDECI_IGRALEC:
                         self.napis.set('Na potezi je rdeči igralec.')
                         self.rdeci_igralec.igraj()
@@ -121,10 +144,11 @@ class Gui():
                         self.napis.set('Na potezi je rumeni igralec.')
                         self.rumeni_igralec.igraj()
                 else:
+                    # Igre je konec
                     self.koncaj_igro(zmagovalec, stirka)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Igrica tri v vrsto")
+    parser = argparse.ArgumentParser(description="Igrica stiri v vrsto")
     parser.add_argument('--debug',
                         action='store_true',
                         help='vklopi sporoÄila o dogajanju')
