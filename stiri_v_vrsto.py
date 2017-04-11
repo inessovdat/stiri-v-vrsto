3669d460150354a282fa8b2bb4406d7ae7c4ab4a
Tajda Šrot <tajda.srot1@gmail.com>
2017-04-11 12:29:29 +0200

    Popravili funkcije za risanje krogcev.

--- stiri_v_vrsto.py
+++ stiri_v_vrsto.py
@@ -1,40 +1,38 @@
 from tkinter import *
 from igra import *
 from clovek import *
+from racunalnik import *
 import argparse
 import logging
+
+MINIMAX_GLOBINA = 3
 
 class Gui():
     TAG_FIGURA = 'figura'
 
     TAG_OKVIR = 'okvir'
 
-    VELIKOST_POLJA = 100
+    VELIKOST_POLJA = 70
 
     # Odmik od roba polja
     ODMIK = 2
-
-    POLMER_KROGCA = VELIKOST_POLJA // 2 - 2
-
-
-    def __init__(self, master):
+    
+    def __init__(self, master, globina):
         self.rdeci_igralec = None
         self.rumeni_igralec = None
+        self.igra = None
 
         master.protocol("WM_DELETE_WINDOW", lambda: self.zapri_okno(master))
 
         # Igralna plošča
-        self.plosca = Canvas(master, width = 7 * Gui.VELIKOST_POLJA + Gui.ODMIK, height = 6 * Gui.VELIKOST_POLJA + Gui.ODMIK, bg = 'blue')
+        self.plosca = Canvas(master, width = 7 * Gui.VELIKOST_POLJA + Gui.ODMIK, height = 6 * Gui.VELIKOST_POLJA + 2 * Gui.ODMIK, bg = 'blue')#barva roba
         self.plosca.grid(row=1, column=0)
         self.plosca.bind("<Button-1>", self.plosca_klik)
         self.narisi_igralno_plosco()
 
-        self.igra = Igra()
-
         # Napis ob začetku igre
-        self.napis = StringVar(master, value="Dobrodošli v 4 v vrsto!")
-        self.w = Label(master, textvariable = self.napis)
-        self.w.grid(row=0, column=0)
+        self.napis = StringVar(master, value="Dobrodošli v 4 v vrsto!")#zakaj ne dela
+        Label(master, textvariable = self.napis).grid(row=0, column=0)
 
         # Glavni menu
         menu = Menu(master)
@@ -43,12 +41,19 @@ class Gui():
         # Podmenu
         menu_igra = Menu(menu, tearoff = 0)
         menu.add_cascade(label="Igra", menu=menu_igra)
-        menu_igra.add_command(label = "Nova igra", command = self.zacni_igro)
+        #menu_igra.add_command(label = "Nova igra", command = self.zacni_igro)
+        menu_igra.add_command(label="Rdeči=Človek, Rumeni=Človek",
+                              command=lambda: self.zacni_igro(Clovek(self), Clovek(self)))
+        menu_igra.add_command(label="Rdeči=Človek, Rumeni=Računalnik",
+                              command=lambda: self.zacni_igro(Clovek(self), Racunalnik(self, Minimax(globina))))
+        menu_igra.add_command(label="Rdeči=Računalnik, Rumeni=Človek",
+                              command=lambda: self.zacni_igro(Racunalnik(self, Minimax(globina)), Clovek(self)))
+        menu_igra.add_command(label="Rdeči=Računalnik, Rumeni=Računalnik",
+                              command=lambda: self.zacni_igro(Racunalnik(self, Minimax(globina)), Racunalnik(self, Minimax(globina))))
 
-        self.zacni_igro()
+        self.zacni_igro(Clovek(self), Racunalnik(self, Minimax(globina)))
 
-
-    def zacni_igro(self):
+    def zacni_igro(self, rdeci_igralec, rumeni_igralec):
         ''' Stanje igre nastavi na začetek.'''
         # Ustavimo vsa vlakna, ki trenutno razmišljajo
         self.prekini_igralce()
@@ -56,8 +61,8 @@ class Gui():
          # Ustvarimo novo igro
         self.igra = Igra()
         # Nastavimo igralce
-        self.rdeci_igralec = Clovek(self)
-        self.rumeni_igralec = Clovek(self)
+        self.rdeci_igralec = rdeci_igralec
+        self.rumeni_igralec = rumeni_igralec
         # Rdeči igralec je prvi na potezi
         self.napis.set("Na potezi je rdeči igralec.")
         self.rdeci_igralec.igraj()
@@ -85,8 +90,6 @@ class Gui():
         self.prekini_igralce()
         master.destroy()
 
-
-
     def narisi_igralno_plosco(self):
         ''' Nariše igralno ploščo.'''
         for i in range(7):
@@ -96,10 +99,9 @@ class Gui():
 
     def narisi_krogec(self, p, barva):
         '''Na ustrezno mesto nariše krogec ustrezne barve.'''
-        (stolpec, sredisce_stolpec, vrstica) = p
-        sredisce_vrstica = vrstica * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
-        self.plosca.create_oval(sredisce_stolpec - Gui.POLMER_KROGCA, sredisce_vrstica - Gui.POLMER_KROGCA,
-                                        sredisce_stolpec + Gui.POLMER_KROGCA, sredisce_vrstica + Gui.POLMER_KROGCA, fill = barva, tag = Gui.TAG_FIGURA)
+        (stolpec, vrstica) = p
+        self.plosca.create_oval(stolpec * Gui.VELIKOST_POLJA + 2 * Gui.ODMIK, vrstica * Gui.VELIKOST_POLJA + 2 * Gui.ODMIK,
+                                (stolpec + 1)* Gui.VELIKOST_POLJA, (vrstica + 1) * Gui.VELIKOST_POLJA, fill = barva, tag = Gui.TAG_FIGURA)
 
     def narisi_zmagovalne_stiri(self, zmagovalec, stirka):
         '''S temnejšo barvo obarva in obrobi zmagovalno štirko.'''
@@ -114,15 +116,20 @@ class Gui():
     def plosca_klik(self, event):
         '''Izvede se ob kliku na ploščo. '''
         stolpec = (event.x - Gui.ODMIK) // Gui.VELIKOST_POLJA
-        sredisce_stolpec = stolpec * Gui.VELIKOST_POLJA + Gui.VELIKOST_POLJA // 2 + Gui.ODMIK
         vrstica = self.igra.vrni_vrstico(stolpec)
-        if self.igra.na_potezi == RDECI_IGRALEC or self.igra.na_potezi == RUMENI_IGRALEC:
-            self.povleci_potezo((stolpec, sredisce_stolpec, vrstica))
+        if vrstica != None:
+            if 0<=vrstica<=5 and 0<=stolpec<=6:
+                if self.igra.na_potezi == RDECI_IGRALEC or self.igra.na_potezi == RUMENI_IGRALEC:
+                    self.povleci_potezo((stolpec, vrstica))
+                else:
+                    # Nihče ni na potezi, ne naredimo nič
+                    pass
         else:
+            # Neveljavna poteza
             pass
 
     def povleci_potezo(self, p):
-        (stolpec, sredisce_stolpec,vrstica) = p
+        (stolpec, vrstica) = p
         igralec = self.igra.na_potezi
         r = self.igra.shrani_poteze(stolpec)
         if r is None:
@@ -133,9 +140,9 @@ class Gui():
             if vrstica != None:
                 # Na zaslon narišemo krogec ustrezne barve
                 if igralec == RDECI_IGRALEC:
-                    self.narisi_krogec(p, 'tomato')
+                    self.narisi_krogec(p, 'brown1')
                 else:
-                    self.narisi_krogec(p, 'gold1')
+                    self.narisi_krogec(p, 'Darkgoldenrod1')
                 # Igre še ni konec
                 (zmagovalec, stirka) = r
                 if zmagovalec == NI_KONEC:
@@ -152,6 +159,10 @@ class Gui():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Igrica štiri v vrsto")
+    parser.add_argument('--globina',
+                        default=MINIMAX_GLOBINA,
+                        type=int,
+                        help='globina iskanja za minimax algoritem')
     parser.add_argument('--debug',
                         action='store_true',
                         help='vklopi sporočila o dogajanju')
@@ -161,5 +172,5 @@ if __name__ == "__main__":
 
     root = Tk()
     root.title("Stiri v vrsto")
-    aplikacija = Gui(root)
+    aplikacija = Gui(root, args.globina)
     root.mainloop()
